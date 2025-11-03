6ae25ecc9ff155d3ba87e65a183a89d8a3a6c081
vnsslprasanna-garimella <gvnsslprasanna@gmail.com>
2025-11-03 21:16:39 +0530

    Time: 257 ms (20.23%), Space: 0B (100%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT id,movie,description,rating FROM cinema WHERE id%2!=0 AND description!='boring' 
+ORDER BY rating DESC;
