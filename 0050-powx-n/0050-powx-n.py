494c887d4c0cad61a7513219953f922df89ebbb4
vnsslprasanna-garimella <gvnsslprasanna@gmail.com>
2026-02-10 22:03:21 +0530

    Time: 1 ms (9.01%), Space: 12.5 MB (33.09%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x, n):
+        # Handle negative power
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        result = 1.0
+
+        while n > 0:
+            if n % 2 == 1:
+                result *= x
+            x *= x
+            n //= 2
+
+        return result
