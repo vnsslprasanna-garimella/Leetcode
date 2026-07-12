e94b263dca4d9678d9e219fc73441a4f112076ff
vnsslprasanna-garimella <gvnsslprasanna@gmail.com>
2026-07-12 21:08:59 +0530

    Time: 69 ms (12.00%) | Memory: 37.5 MB (80.09%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        # Store the rank for each number in arr
+        num_to_rank = {}
+        sorted_arr = sorted(arr)
+        rank = 1
+        for i in range(len(sorted_arr)):
+            if i > 0 and sorted_arr[i] > sorted_arr[i - 1]:
+                rank += 1
+            num_to_rank[sorted_arr[i]] = rank
+        for i in range(len(arr)):
+            arr[i] = num_to_rank[arr[i]]
+        return arr
